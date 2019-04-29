9882240ca420379bfedf30e0f1b78ad5277cf6a4
mengoreo <mengoreo@163.com>
2019-04-29 22:40:02 +0800

    for my sync

--- configurations/elpy-config.el
+++ configurations/elpy-config.el
@@ -5,5 +5,6 @@
 	(setq elpy-shell-use-project-root nil)
 	(setq python-shell-completion-native-enable nil)
 	(setq python-shell-interpreter "/opt/local/bin/python")
+	(add-hook 'before-save-hook 'elpy-autopep8-fix-code)
 	)
 (provide 'elpy-config)
